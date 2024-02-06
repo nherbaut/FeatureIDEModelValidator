a80d8debc569c38711a94affa4d00ffbb5c4cb00
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2024-02-06 01:22:16 +0100

    Model Validator

---
+++ run.sh
@@ -0,0 +1,1 @@
+java  -cp ./lib/gson-2.10.1.jar:./lib/antlr-3.4.jar:./lib/commons-math-2.2.jar:./lib/de.ovgu.featureide.lib.fm-v3.9.3.jar:./lib/org.sat4j.core.jar:./lib/org.sat4j.pb.jar:./lib/splar.jar:./lib/SPLCAT.jar:./lib/uvl-parser.jar:./lib/de.ovgu.featureide.fm.core-3.9.3-SNAPSHOT.jar de.ovgu.featureide.fm.core.cli.FeatureIDECLI  validateModel -fm ./model.xml  -o results.json > /dev/null
